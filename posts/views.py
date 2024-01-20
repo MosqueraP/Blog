6b20114c51f3bfec9dd5ca86e9af92c610a06a0d
Plinio <mcplinio.15@gmail.com>
2024-01-19 22:05:36 -0500

    añadir formulario de crear y y redireccion

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from posts.models import Post, PostView, Like, Comment
@@ -15,13 +16,27 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     form_class = PostForm
     model = Post
-
+    success_url = '/' # redireccionar a la misma vista en la que se invoca
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type':'create'
+        })
+        return context
 
 # Actualizar
 class PostUpdateView(UpdateView):
     form_class = PostForm
     model = Post
-    
+    success_url = '/' # redireccionar a la misma vista en la que se invoca
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type':'update'
+        })
+        return context
 
 
 class PostDeleteView(DeleteView):
