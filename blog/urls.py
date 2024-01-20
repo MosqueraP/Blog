7f25d93048ea8a5c264b15118367c221d3965dbd
Plinio <mcplinio.15@gmail.com>
2024-01-19 20:22:55 -0500

    crear las vistas basadas en clase, y configurar las urls y vistas

--- blog/urls.py
+++ blog/urls.py
@@ -18,10 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import (
+    PostLisView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView)     
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostLisView.as_view(), name='list'),
+    path('<slug>', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('create/', PostCreateView.as_view(), name='create'),
 ]
 
 if settings.DEBUG:
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from posts.models import Post, PostView, Like, Comment
+
+from posts.models import Post
 
 # Create your views here.
+
+class PostLisView(ListView):
+    model = Post
+    
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+class PostDeleteView(DeleteView):
+    model = Post
