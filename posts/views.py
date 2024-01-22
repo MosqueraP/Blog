e2932020171c573c22658f35bc29462a293993f9
Plinio <mcplinio.15@gmail.com>
2024-01-21 21:05:12 -0500

    agegar formulario crispy y estilos en post_form.html

--- posts/views.py
+++ posts/views.py
@@ -27,7 +27,9 @@ class PostDetailView(DetailView):
             comment.save()
             return redirect('detail', slug=post.slug)
 
-    
+        return redirect('detail', slug=self.get_object().slug)
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
